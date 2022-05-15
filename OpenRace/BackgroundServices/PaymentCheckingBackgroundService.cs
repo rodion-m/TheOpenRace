16fc6ce5812e4918adf2ba7b9ee8e8fd5c5dadfa
Rodion Mostovoy <36400912+rodion-m@users.noreply.github.com>
2022-05-16 00:25:12 +0300

    Add retries to payments checking logic

--- OpenRace/BackgroundServices/PaymentCheckingBackgroundService.cs
+++ OpenRace/BackgroundServices/PaymentCheckingBackgroundService.cs
@@ -3,31 +3,45 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using NodaTime;
 using OpenRace.Data;
 using OpenRace.Data.Ef;
 using OpenRace.Data.GSL;
+using OpenRace.Entities;
 using OpenRace.Exceptions;
 using OpenRace.Features.Payment;
 using OpenRace.Features.Registration;
+using Polly;
 
 namespace OpenRace.BackgroundServices;
 
 public class PaymentCheckingBackgroundService : BackgroundService
 {
     private readonly IGenericServiceProvider<MembersRepository, PaymentService, RegistrationService> _services;
+    private readonly ILogger<PaymentCheckingBackgroundService> _logger;
 
     public PaymentCheckingBackgroundService(
-        IGenericServiceProvider<MembersRepository, PaymentService, RegistrationService> services)
+        IGenericServiceProvider<MembersRepository, PaymentService, RegistrationService> services,
+        ILogger<PaymentCheckingBackgroundService> logger)
     {
         _services = services;
+        _logger = logger;
     }
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         var timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
         while (await timer.WaitForNextTickAsync(stoppingToken))
         {
-            await CheckPayments();
+            try
+            {
+                await CheckPayments();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error while checking payments");
+            }
         }
     }
 
@@ -35,15 +49,37 @@ public class PaymentCheckingBackgroundService : BackgroundService
     {
         await using var scope = _services.CreateScope();
         var (membersRepository, paymentService, membersService) = scope;
-
         var members = await membersRepository.GetUnpaidMembers();
         foreach (var member in members)
         {
-            var paid = await paymentService.IsPaymentPaid(member.Payment!.Id);
+            var paid = await IsPaymentPaidWithRetry(member, paymentService);
             if (paid)
             {
                 await membersService.SetMembershipPaid(member);
             }
         }
+    }
+
+    private async Task<bool> IsPaymentPaidWithRetry(Member member, PaymentService paymentService)
+    {
+        var paymentId = member.Payment!.Id;
+        var policy = Policy
+            .Handle<Exception>()
+            .WaitAndRetryAsync(3, retryCount => TimeSpan.FromSeconds(Math.Pow(2, retryCount)),
+                (exception, _) =>
+                {
+                    _logger.LogWarning(exception, "Error occured while checking payment {PaymentId}", paymentId);
+                });
+        var res = await policy.ExecuteAndCaptureAsync(() => paymentService.IsPaymentPaid(paymentId));
+        if (res.Outcome == OutcomeType.Failure)
+        {
+            _logger.LogError(
+                res.FinalException,
+                "Error occured while checking payment {PaymentId}, MemberId: {MemberId}",
+                paymentId, member.Id
+            );
+        }
+
+        return res.Result;
     }
 }
