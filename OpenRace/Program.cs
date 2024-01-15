410c9ff97b13f3606d31f9f8bfcbd9a4cc77de2d
Rodion Mostovoi <rodion.mostovoy@outlook.com>
2024-01-15 15:02:54 +0800

    Remove unused Serilog imports in Program.cs
    The unused imports for `Serilog.Events` and `Serilog.Formatting.Compact` in the Program.cs file in

--- OpenRace/Program.cs
+++ OpenRace/Program.cs
@@ -2,8 +2,6 @@ using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using Serilog.Events;
-using Serilog.Formatting.Compact;
 
 namespace OpenRace
 {
